7ac2b323d2b1ee77ecb38de22feb2cb6a6c21ec3
copperhammer <sw200451@sina.com>
2019-07-18 15:41:02 +0800

    MVC

---
+++ MVC/models/user_role.py
@@ -0,0 +1,31 @@
+import json
+from enum import (
+    Enum,
+    auto,
+)
+
+
+class UserRole(Enum):
+    guest = auto()
+    normal = auto()
+
+    def translate(self, _escape_table):
+        return self.name
+
+
+class CopEncoder(json.JSONEncoder):
+    prefix = "__enum__"
+
+    def default(self, o):
+        if isinstance(o, UserRole):
+            return {self.prefix: o.name}
+        else:
+            return super().default(o)
+
+
+def Cop_decode(d):
+    if CopEncoder.prefix in d:
+        name = d[CopEncoder.prefix]
+        return UserRole[name]
+    else:
+        return d
---
+++ MVC/secret.py
@@ -0,0 +1,1 @@
+mysql_password='Web*2019'
